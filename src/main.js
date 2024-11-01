7dd9ece94b2f4430bc2c7cf23bb325b6636fa1d1
Saad el quaul <saadelquaul@gmail.com>
2024-11-01 17:15:03 +0100

    tasks cards for each status added and hidded to be visible by js in the next commits

---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+const allTasksBtn = document.getElementsByClassName("tasksBtn");
+const toDoBtn = document.getElementsByClassName("toDoBtn");
+const inProgressBtn = document.getElementsByClassName("inPBtn");
+const doneBtn = document.getElementsByClassName("doneBtn");
+const searchIconBtn = document.getElementById("search");
+const seachBar = document.getElementsByClassName("search");
+const addTaskBtn = document.getElementsByClassName("addTask");
+const addBtn = document.getElementsByClassName("add");
+const cancelBtn = document.getElementsByClassName("cancel");
+const AddTaskInfoForm = document.getElementsByClassName("addTaskInfo");
+const toDoSection = document.getElementsByClassName()
